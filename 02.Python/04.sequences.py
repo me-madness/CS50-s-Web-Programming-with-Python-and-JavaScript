a5efed8144ebe59924f863aca5b7e33bed2d3178
me_madness <p_rangel@abv.bg>
2024-08-16 16:11:51 +0200

    04.sequences.py is done

---
+++ 02.Python/04.sequences.py
@@ -0,0 +1,15 @@
+# First way
+name = "Harry"
+
+print(name)
+
+# Second way
+names = ["Harry", "Ron", "Harmione"]
+
+print(names[0])
+
+# Third way
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
