f135b5d2007b7800afe857ac9641f90ef5ec3ad6
me_madness <p_rangel@abv.bg>
2024-08-16 16:23:48 +0200

    07.loops.py is done

---
+++ 02.Python/07.loops.py
@@ -0,0 +1,13 @@
+# First Way
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+    
+# Second Way
+for i in range(6):
+    print(i)
+    
+# Third Way
+names = ["Harry", "Ron", "Harmione", "Ginny"]
+
+for name in names:
+    print(name)            
