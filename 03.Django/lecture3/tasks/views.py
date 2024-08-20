bc3fdbc60b6bc5752db805e4ed582fd6c7b5efff
me_madness <p_rangel@abv.bg>
2024-08-20 13:21:09 +0200

    a lot changes in views file

--- 03.Django/lecture3/tasks/views.py
+++ 03.Django/lecture3/tasks/views.py
@@ -1,7 +1,13 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
 
+
+class NewTaskForm(forms.Form):
+    task = forms.Charfield(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
@@ -10,4 +16,6 @@ def index(request):
     
     
 def add(request):
-    return render(request, "tasks/add.html")    
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm
+    })    
