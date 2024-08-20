2434cda81c2efb1afd349d174a9b94f34c8e79ce
me_madness <p_rangel@abv.bg>
2024-08-20 10:24:59 +0200

    a new changes in my views file

--- 03.Django/lecture3/hello/views.py
+++ 03.Django/lecture3/hello/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello!")
+
+
+def rangel(request):
+    return HttpResponse("Hello, Rangel!")
