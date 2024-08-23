1155654399818f60cbf9a16a500fc5c391258d61
me_madness <p_rangel@abv.bg>
2024-08-23 15:54:57 +0200

    first migration is done

--- 04.SQL, Models and Migrations/airline/flights/models.py
+++ 04.SQL, Models and Migrations/airline/flights/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Flight(models.Model):
     origin = models.CharField(
-        max_length=64,
+        max_length=64
     )
     
     destination = models.CharField(
-        max_length=64,
+        max_length=64
     )
     
     duration = models.IntegerField()
