9cecddd599d87249403e24c795f47e0f6c1d15b8
me_madness <p_rangel@abv.bg>
2024-08-30 21:06:32 +0200

    new commit after a few cjanges in my views.py file

--- 04.SQL, Models and Migrations/airline/flights/views.py
+++ 04.SQL, Models and Migrations/airline/flights/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Flight, Passenger
 
@@ -20,3 +22,5 @@ def book(request, flight_id):
     if request.method == "POST":
         flight = Flight.objects.get(pk=flight_id)
         passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id, )))
