5e65971ad50cbbf8df228ca7c787fda27fb9c3be
me_madness <p_rangel@abv.bg>
2024-09-04 19:51:02 +0200

    even after refresh my page remember my data because of the function local storage

--- 05.JavaScript/counter.js
+++ 05.JavaScript/counter.js
@@ -10,5 +10,6 @@ function count() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
 });
